08bbdccad1ab4151a0576c2108e6b68c6c82f641
KJM <kjm99d@naver.com>
2021-01-17 19:09:48 +0900

    Inital Project

---
+++ src/main/java/com/project/customwol/CustomWolApplication.java
@@ -0,0 +1,13 @@
+package com.project.customwol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomWolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomWolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/customwol/CustomWolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.customwol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomWolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
