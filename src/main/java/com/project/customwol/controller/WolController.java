936aca215af98098c8014cc41818df70884a3853
KJM <kjm99d@naver.com>
2021-01-27 04:23:36 +0900

    form 부분 추가

--- src/main/java/com/project/customwol/controller/WolController.java
+++ src/main/java/com/project/customwol/controller/WolController.java
@@ -12,7 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.PrintWriter;
 import java.util.List;
 
 @Controller
@@ -27,6 +29,17 @@ public class WolController {
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping("/error")
+    public String errorPage() {
+        return "wol/error";
+    }
+
+    /**
+     * 리스트 맵핑
+     * @param model
+     * @param authentication
+     * @return
+     */
     @GetMapping("/list")
     public String list(Model model, Authentication authentication) {
         /* 사용자 정보 가져오기 */
@@ -35,21 +48,38 @@ public class WolController {
 
         List<Wol> wols = wolRepository.findAllByUser_id(user.getId());
 
-        for (Wol wol: wols) {
-            System.out.println(wol.getId());
-            System.out.println(wol.getMac());
-            System.out.println(wol.getDescription());
-
-        }
-
         model.addAttribute("wols", wols);
         return "wol/list";
     }
 
-    @PostMapping("/list")
+    @PostMapping("/form")
     public String list(Wol wol, Authentication authentication) {
         String username = authentication.getName();
+        System.out.println(wol.getId() + ", " + wol.getMac() + ", " + wol.getDescription());
+
         wolService.save(username, wol);
-        return "redirect:/list";
+        return "redirect:/wol/list";
+    }
+
+    /**
+     * BindingResult 파라미터는 validator에서 처리한다.
+     * @param model
+     * @param id
+     * @return
+     */
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = true) Long id, Authentication authentication) {
+        Wol wol = wolRepository.findById(id).orElse(null);
+        model.addAttribute("wol", wol);
+
+
+        String username = authentication.getName();
+        User user = userRepository.findByUsername(username);
+
+        if (wol.getUser().getId() != user.getId()) {
+            return "redirect:/wol/error";
+        }
+
+        return "wol/form";
     }
 }
